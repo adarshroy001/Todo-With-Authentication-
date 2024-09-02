02f2b95d3c0773fdbd621c8183f600b828929c6e
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2024-09-02 20:08:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ import cors from "cors" ;
  app.use(express.json()) ; 
  app.use(cookieParser()) ; 
  app.use(cors({
+  
    origin: [process.env.FRONTEND_URL],
    methods:["GET" , "POST" ,"PUT", "DELETE"],
    credentials: true 
