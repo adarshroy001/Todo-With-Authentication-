0446be135946e201d39d5f4ed29779313308a2f1
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2024-09-02 19:00:32 +0530

    Update cookieProvider.js

--- Utility/cookieProvider.js
+++ Utility/cookieProvider.js
@@ -1,18 +1,19 @@
 import jwt from "jsonwebtoken";
 export const SendCookies = (user,res,message,statusCode=200)=>{
-    const token = jwt.sign({_id: user._id},process.env.JWT_SECRET) ; 
-    res
-    .status(statusCode)
-    .cookie("token",token,{
-        httpOnly:true ,
-        maxAge: 15*60*60*1000 ,
-        sameSite: process.env.NODE_ENV === "Development" ? "lax" :  "none" , 
-        secure: process.env.NODE_ENV === "Development" ? false : true ,
+ const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
 
-    }).json({
-    success: true , 
-    message,
-
-})
+res.status(statusCode)
+   .cookie("token", token, {
+        httpOnly: true,
+        maxAge: 15 * 60 * 60 * 1000, // 15 hours
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Development" ? false : true,
+        // domain: process.env.DOMAIN || "yourdomain.com", // Optional: Specify domain if needed
+        // path: "/", // Optional: Specify the path if needed
+   })
+   .json({
+        success: true,
+        message,
+   });
 
 }
