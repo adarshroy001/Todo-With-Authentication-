8ecb052f1b9fc4b7f567c7a7d789573e1a1b2f37
Adarsh Roy <adarshroy2010@gmail.com>
2024-09-03 21:52:22 +0530

    phj

--- controllers/user.js
+++ controllers/user.js
@@ -59,16 +59,14 @@ export const getMyProfile = (req,res)=> {
     })
 } ;
 
-export const logout =  (req , res) => {
-    res.status(200) .cookie("token","",{
-        expires: new Date(Date.now()),
-        sameSite: process.env.NODE_ENV === "Development" ? "lax" :  "none" , 
-        secure: process.env.NODE_ENV === "Development" ? false : true ,
-    }) .json({
-        success: true , 
-        message: "logout Sucessfully" , 
-        user: req.user
-    })
-
-
+export const logout = (req, res) => {
+    res.status(204).cookie("token", "", {
+        expires: new Date(Date.now()), // Immediately expire the cookie
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Development" ? false : true,
+        httpOnly: true, // Add this to enhance security
+    }).json({
+        success: true,
+        message: "Logout successfully",
+    });
 };
