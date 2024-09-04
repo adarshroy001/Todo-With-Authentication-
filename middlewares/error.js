c6002760a2d07c0e9ef365d8924d9432690782c7
Adarsh Roy <adarshroy2010@gmail.com>
2024-09-04 23:47:03 +0530

    push

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,33 +1,23 @@
-class ErrorHandler extends Error {
-   constructor(message, statusCode) {
-     super(message);
-     this.statusCode = statusCode;
- 
-     // Capture the stack trace (useful for debugging)
-     if (Error.captureStackTrace) {
-       Error.captureStackTrace(this, this.constructor);
-     }
-   }
+ class ErrorHandler extends Error {
+    constructor (message , statusCode){
+        super(message) ; 
+        this.statusCode  = statusCode  ;
+    }
  }
  
- export const errorMiddleware = (err, req, res, next) => {
-   // Set default values for message and status code if not provided
-   err.message = err.message || "Internal Server Error";
-   err.statusCode = err.statusCode || 500;
- 
-   // Log error in development mode for debugging purposes
-   if (process.env.NODE_ENV === "development") {
-     console.error(err.stack);
-   }
- 
-   // Send error response
-   return res.status(err.statusCode).json({
-     success: false,
-     message: err.message,
-     ...(process.env.NODE_ENV === "development" && { stack: err.stack }), // Only include stack in development
-   });
- };
- 
- // Export the ErrorHandler as a named export
- export { ErrorHandler };
+ export  const errorMidddleware = (err,req , res ,next)=>{
+    err.message = err.message || "Internal Server Error" ; 
+    err.statusCode = err.statusCode || 500 ; 
+
+
+ return res.status(err.statusCode).json({
+    success: false , 
+    message: err.message ,
+ });
+
+ }
+
+ export default ErrorHandler ; 
+
+
  
