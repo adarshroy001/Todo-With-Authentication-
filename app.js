b726395655d3b92e15433fe789168fa1b2f2611d
Adarsh Roy <adarshroy2010@gmail.com>
2024-09-07 16:52:12 +0530

    push

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import cors from "cors" ;
  app.use(express.json()) ; 
  app.use(cookieParser()) ; 
  app.use(cors({
-   origin:  [process.env.FRONTEND_URL || "https://todo-by-adarsh.netlify.app"],
+   origin:  [process.env.FRONTEND_URL , "http://localhost:5173"  ],
    methods:["GET" , "POST" ,"PUT", "DELETE"],
    credentials: true 
  }))  ;
